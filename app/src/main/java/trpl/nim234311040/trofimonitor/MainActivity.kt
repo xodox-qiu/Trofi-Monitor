090e885144fb632d4900b101672f0bb08df89152
xodox-qiu <dvalinnsimulacrum@gmail.com>
2024-10-23 06:13:49 +0700

    Initial commit - TrofiMonitor by NIM 234311040

--- app/src/main/java/trpl/nim234311040/trofimonitor/MainActivity.kt
+++ app/src/main/java/trpl/nim234311040/trofimonitor/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
